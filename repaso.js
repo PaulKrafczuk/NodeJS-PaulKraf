4ede922f36ea9343ef8cb4381dfeb7dedbcf3cea
PaulKrafczuk <krafczukpaul@gmail.com>
2019-11-05 20:53:53 -0300

    app pizas cliente y pedidos

---
+++ repaso.js
@@ -0,0 +1,15 @@
+//arrow functions
+let elDoble = num=>num*2;
+
+console.log(elDoble(6));
+
+// calback
+
+let sumar = (numero1, numero2) => numero1 + numero2;
+let restar = (numero1, numero2) => numero1 - numero2;
+let dividir = (numero1, numero2) => numero1 / numero2;
+let multiplicar = (numero1, numero2) => numero1 * numero2;
+
+let calculadora= (numero1, numero2, operacion);
+
+console.log(calculadora(3, 2, restar));
